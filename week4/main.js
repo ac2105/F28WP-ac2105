08ea0834023fe7e5e8c8a802a256f69305e46487
ac2105 <114248296+ac2105@users.noreply.github.com>
2023-11-03 09:48:41 +0400

    created a new file

---
+++ week4/main.js
@@ -0,0 +1,14 @@
+var mycity = [
+    {
+        "Name": "Dubai",
+        "Country": "UAE",
+        "Place": "Burj Khalifa"
+    },
+    {
+        "Name": "Milan",
+        "Country": "Italy",
+        "Place": "Duomo Di Milano"
+    
+    }
+]
+console.log(mycity[1].Country);
