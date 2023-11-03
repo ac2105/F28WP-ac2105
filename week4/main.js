5c66ba50b0ef095757f8abd9a4365d485af35978
ac2105 <114248296+ac2105@users.noreply.github.com>
2023-11-03 09:50:09 +0400

    new

--- week4/main.js
+++ week4/main.js
@@ -8,7 +8,11 @@ var mycity = [
         "Name": "Milan",
         "Country": "Italy",
         "Place": "Duomo Di Milano"
-    
+    },
+    {
+        "Name": "Cape Town",
+        "Country": "South Africa",
+        "Place": "Table Mountain"   
     }
 ]
 console.log(mycity[1].Country);
